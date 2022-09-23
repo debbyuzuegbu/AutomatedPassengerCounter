3b2c8c1fb2a50875e6bb0627ddfabae25c25951f
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-09-23 23:28:32 +0100

    added style to the navbar

---
+++ info-site/src/header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="../images/icons8-react-16.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>ReactFacts</li>
+                    <li>React Course - Project 1</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
