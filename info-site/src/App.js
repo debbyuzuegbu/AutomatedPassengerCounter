ad3d6a1e242d28318c439467d0d959327c575ca6
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-09-24 13:16:14 +0100

    updated main content

--- info-site/src/App.js
+++ info-site/src/App.js
@@ -2,15 +2,15 @@ import React  from "react"
 
 export default function App() {
     return (
-        <div>
-            <h1>Fun facts about React </h1> 
-            <ul>
+        <main>
+            <h1 className="main--title">Fun facts about React </h1> 
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>His well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>                                                                                                                                                                                                                                                          
-        </div>
+        </main>
     )
 }
