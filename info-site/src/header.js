41c207426e0c230034ab59fb30455fd84e07f58c
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-09-24 01:22:00 +0100

    styled navbar

--- info-site/src/header.js
+++ info-site/src/header.js
@@ -4,11 +4,9 @@ export default function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src="../images/icons8-react-16.png" className="nav-logo" />
-                <ul className="nav-items">
-                    <li>ReactFacts</li>
-                    <li>React Course - Project 1</li>
-                </ul>
+                <img src="../images/icons8-react-16.png" className="nav--logo" />
+                <h3 className="nav--logo_text">ReactFacts</h3>
+                <h4 className="nav--title">React Course - Project 1</h4>
             </nav>
         </header>
     )
