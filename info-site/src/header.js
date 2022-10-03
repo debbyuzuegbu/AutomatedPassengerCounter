f2925226e3ae843ae00ee9c422fcce3514788314
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-10-03 14:34:14 +0100

    changed prop name for image in header component

--- info-site/src/header.js
+++ info-site/src/header.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Babe from "./images/logo192.png"
+import Logo from "./images/logo192.png"
 
 export default function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src={Babe} className="nav--logo" />
+                <img src={Logo} className="nav--logo" />
                 <h3 className="nav--logo_text">ReactFacts</h3>
                 <h4 className="nav--title">React Course - Project 1</h4>
             </nav>
